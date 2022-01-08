a67fe721b5242fd67eec9dd1f816f533ac029575
Yoav Ganbar <yoav.ganbar@fiverr.com>
2022-01-08 11:56:55 +0200

    change links + add prettier + run

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,29 +1,26 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to FedBites!</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
+          <a target="_blank" href="https://anchor.fm/fedbites">
+            Podcast
           </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
+          <a target="_blank" href="https://medium.com/fedbites">
+            Medium Publication
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+          <a target="_blank" href="https://dev.to/fedbites">
+            FedBites on Dev.to
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://twitter.com/fedbites">
+            FedBites on Twitter
           </a>
         </li>
       </ul>
